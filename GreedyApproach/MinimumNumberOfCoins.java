e7c41226b8fe362f662d7345e7b14de03f331d1d
Sachin-Gola <sachingola343@gmail.com>
2025-07-02 10:08:24 +0530

    Fisrt Solution Commited

---
+++ GreedyApproach/MinimumNumberOfCoins.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MinimumNumberOfCoins {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int amount = sc.nextInt();
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        System.out.println(coinChange(coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int count = 0;
+        int currCoin = coins.length - 1;
+        while (amount > 0 && currCoin >= 0) {
+            if (coins[currCoin] <= amount) {
+                amount -= coins[currCoin];
+                count++;
+            } else {
+                currCoin--;
+            }
+        }
+        return count;
+    }
+}
---
+++ GreedyApproach/Problem1_BuyChocolates.java
@@ -0,0 +1,5 @@
+public class Problem1_BuyChocolates {
+    public static void main(String[] args) {
+        
+    }
+}
