726f3c78c8d153f27f0620ea0d3eb11230524749
Sachin-Gola <sachingola343@gmail.com>
2025-07-08 09:52:30 +0530

    New Problems Commited

--- DynamicProgramning/CoinExchange.java
+++ DynamicProgramning/CoinExchange.java
@@ -1,28 +1,21 @@
 import java.util.Arrays;
 public class CoinExchange{
     public static void main(String[] args) {
-        int[] arr={9,5,1,4};
-        Arrays.sort(arr);
-        int amount=4;
-        int[] cp=new int[amount+1];
-        int[] sp=new int[amount+1];
-
-        cp[0]=0;
-        sp[0]=0;
-        for (int p = 1; p <= amount; p++) {
-            int minCoinsRequired = Integer.MAX_VALUE;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] <= p && cp[p - arr[i]] != Integer.MAX_VALUE) {
-                    if (cp[p - arr[i]] + 1 < minCoinsRequired) {
-                        minCoinsRequired = cp[p - arr[i]] + 1;
-                        sp[p] = arr[i]; 
-                    }
-                }
+        int[] coins={9,5,1,4};
+        Arrays.sort(coins);
+        int amount=11;
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]= 0;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j]<=i) dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
             }
-            cp[p] = minCoinsRequired;
+        }
+        if(dp[amount]>amount) System.out.println(-1);
+        else System.out.println(dp[amount]);
         
-    }
-    System.out.println( cp[cp.length-1]);
+    
   }
 
 }
